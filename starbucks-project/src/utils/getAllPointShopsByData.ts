616a89d8e3c75f0ae78c57663fe48200af0c22ea
yakov cohen <yakovc1431@gmail.com>
2022-06-13 13:20:43 +0300

    Create getAllPointShopsByData.ts

---
+++ starbucks-project/src/utils/getAllPointShopsByData.ts
@@ -0,0 +1,30 @@
+// ol
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import { fromLonLat } from 'ol/proj';
+import { Icon, Style } from 'ol/style';
+// types
+import { shopType } from '../vite-env';
+
+// getAllPointShopsByData
+export const getAllPointShopsByData = (data: shopType[]) => {
+  const allPointShops: Feature<Point>[] = [];
+
+  data.forEach(shop => {
+    const pointShop = new Feature({
+      geometry: new Point(fromLonLat([shop.longitude, shop.latitude])),
+    });
+    pointShop.setStyle(
+      new Style({
+        image: new Icon({
+          src: '../../src/data/starbucks-logo.png',
+          scale: 0.1,
+          opacity: 0.75,
+        }),
+      })
+    );
+    allPointShops.push(pointShop);
+  });
+
+  return allPointShops;
+};
