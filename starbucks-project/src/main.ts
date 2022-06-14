d3133d343179efff35aeec286bbb820062ccb193
yakov cohen <yakovc1431@gmail.com>
2022-06-14 12:25:28 +0300

    Add pop up shop

--- starbucks-project/src/main.ts
+++ starbucks-project/src/main.ts
@@ -105,6 +105,44 @@ export const map = new Map({
 
 // * * * create map * * *
 
-map.on('click', function (evt) {
-  console.log(evt.coordinate);
+//  * * * Add Popup Shop * * *
+const shopInfo = document.getElementById('shop-info');
+
+map.on('pointermove', function (evt) {
+  if (evt.dragging) {
+    return;
+  }
+
+  const pixel = map.getEventPixel(evt.originalEvent);
+  const hit = map.hasFeatureAtPixel(pixel);
+  map.getTargetElement().style.cursor = hit ? 'pointer' : '';
+
+  if (hit) {
+    const feature = map.forEachFeatureAtPixel(pixel, function (feature: any) {
+      return feature;
+    });
+
+    if (feature) {
+      const shop = shopsData.filter(
+        shop => shop.store_id === feature.values_.id_
+      )[0];
+
+      if (shop) {
+        shopInfo!.innerHTML = `
+        <div>
+          <span>name: ${shop.name}</span>
+          <span>city: ${shop.city}</span>
+        </div>`;
+
+        shopInfo!.style.visibility = 'visible';
+        shopInfo!.style.top = `${evt.pixel[1] + 80}px`;
+        shopInfo!.style.left = `${evt.pixel[0] + 10}px`;
+      } else {
+        shopInfo!.style.visibility = 'hidden';
+      }
+    }
+  } else {
+    shopInfo!.style.visibility = 'hidden';
+  }
 });
+//  * * * Add Popup Shop * * *
